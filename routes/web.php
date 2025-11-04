4d5b6cd69aa5e6d1537e29ab42eb9867ddd8b9dc
BenjCaro <carbenjamin@gmail.com>
2025-11-04 11:13:17 +0100

    creation Core\Container pour injecter les dépendances dans les controllers.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,21 @@
 <?php
 
+use Carbe\Petitcreuxv2\Core\Container;
 use Carbe\Petitcreuxv2\Controllers\HomeController;
+use Carbe\Petitcreuxv2\Controllers\UserController;
+
+$container = new Container();
+
 
 $router->map('GET', '/', function() {
     
     $home = new HomeController();
     $home->viewHomePage();
 
+});
+
+$router->map('GET', '/login', function() use ($container) {
+    
+    $login = $container->get(UserController::class);
+    $login->loginForm();
 });
