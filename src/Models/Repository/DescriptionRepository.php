64af84d30427da55322e8e7558b4964a70e26146
BenjCaro <carbenjamin@gmail.com>
2025-11-14 18:02:55 +0100

    createRecipe pour le DescriptionRepo

---
+++ src/Models/Repository/DescriptionRepository.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace Carbe\Petitcreuxv2\Models\Repository;
+use Carbe\Petitcreuxv2\Models\Entites\Description;
+
+class DescriptionRepository extends BaseRepository {
+
+    protected string $table = "descriptions";
+    
+    public function __construct()
+    {
+        return parent::__construct();
+    }
+
+    public function createRecipe(int $idRecipe, Description $description) :bool{
+       $stmt = $this->pdo->prepare("INSERT INTO {$this->table} (step_number, texte, id_recipe)
+       VALUES (:step_number, :texte, :id_recipe)");
+       return  $stmt->execute([
+            'step_number' => $description->getStepNumber(),
+            'texte' => $description->getTexte(),
+            'id_recipe' => $idRecipe
+       ]);
+    } 
+    
+
+}
